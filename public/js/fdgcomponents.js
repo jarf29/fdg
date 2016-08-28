4a52b1b83d055f41754174eef77495d9fffc1f0f
Andreslt <andreslastrat@gmail.com>
2016-08-28 11:13:21 -0500

    Modal updated

--- public/js/fdgcomponents.js
+++ public/js/fdgcomponents.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-   $('edit-ticket').on('click', function(){
-       
+   $('.view-ticket').on('click', function(){
+            $('#edit-ticket-id').val($(this).data('id'));
+            $('#edit-ticket-number').val($(this).data('number'));
+            $('#edit-ticket-title').val($(this).data('title'));
+            $('#edit-ticket-description').val($(this).data('description'));
+            $('#edit-ticket-store').val($(this).data('store'));
+            $('#edit-ticket-city').val($(this).data('city'));
    });
 });
